9f96b21a6a10fa717c2f3e7296f50dc1ee827aa7
pol-kom <polkomtel@gmail.com>
2025-02-28 13:13:04 +0100

    OK

---
+++ src/main/java/kp/Constants.java
@@ -0,0 +1,16 @@
+package kp;
+
+/**
+ * The constants.
+ *
+ */
+@SuppressWarnings("doclint:missing")
+public final class Constants {
+	public static final String OPTION_PANE_BACKGROUND = "OptionPane.background";
+	public static final String PANEL_BACKGROUND = "Panel.background";
+	public static final String GO_BACK_LABEL = "Go Back";
+
+	private Constants() {
+		throw new IllegalStateException("Utility class");
+	}
+}
