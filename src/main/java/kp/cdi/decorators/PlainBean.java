17e7e10e6aa8bc70461262e5b208793855ee6936
pol-kom <polkomtel@gmail.com>
2025-02-28 13:13:13 +0100

    OK

---
+++ src/main/java/kp/cdi/decorators/PlainBean.java
@@ -0,0 +1,13 @@
+package kp.cdi.decorators;
+
+/**
+ * The plain bean with the content.
+ */
+public interface PlainBean {
+    /**
+     * Shows content.
+     *
+     * @param content the content
+     */
+    void show(String content);
+}
