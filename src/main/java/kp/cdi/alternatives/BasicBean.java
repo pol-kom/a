e00f9e59644306fbed9f34ddbfcf53dd61ff1a40
pol-kom <polkomtel@gmail.com>
2025-02-28 13:13:07 +0100

    OK

---
+++ src/main/java/kp/cdi/alternatives/BasicBean.java
@@ -0,0 +1,15 @@
+package kp.cdi.alternatives;
+
+/**
+ * The basic bean.
+ *
+ */
+public interface BasicBean {
+
+	/**
+	 * Shows content.
+	 * 
+	 * @param content the content
+	 */
+	void show(String content);
+}
