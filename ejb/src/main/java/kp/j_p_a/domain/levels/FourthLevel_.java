6520958a654f648d21ddef1183adf04f9b5ea9d6
pol-kom <polkomtel@gmail.com>
2025-03-08 11:21:40 +0100

    OK

---
+++ ejb/src/main/java/kp/j_p_a/domain/levels/FourthLevel_.java
@@ -0,0 +1,17 @@
+package kp.j_p_a.domain.levels;
+
+import jakarta.persistence.metamodel.StaticMetamodel;
+
+/**
+ * Metamodel for the {@link FourthLevel} entity class.
+ */
+@SuppressWarnings("java:S101") // switch off Sonarqube rule 'class naming convention'
+@StaticMetamodel(FourthLevel.class)
+public class FourthLevel_ extends Level_ {
+    /**
+     * Default constructor.
+     */
+    public FourthLevel_() {
+        // constructor is empty
+    }
+}
