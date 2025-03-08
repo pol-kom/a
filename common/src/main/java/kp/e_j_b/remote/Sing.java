4bd73c54a9c676e3938b0c0d5144188b6d3770e3
pol-kom <polkomtel@gmail.com>
2025-03-08 11:20:56 +0100

    OK

---
+++ common/src/main/java/kp/e_j_b/remote/Sing.java
@@ -0,0 +1,14 @@
+package kp.e_j_b.remote;
+
+import jakarta.ejb.Remote;
+import kp.e_j_b.Common;
+
+/**
+ * The <b>singleton</b> session bean. Extend the {@link Common} interface.
+ * <p>
+ * This is the {@link Remote} business interface.
+ * </p>
+ */
+@Remote
+public interface Sing extends Common {
+}
